4ddd51fbbe193ef58459ed2433901350e00cda1d
Johan Svensson <johan@atomicplayboy.net>
2016-11-07 17:02:54 +0100

    Add project files.

---
+++ Josephus/CircularList.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Josephus
+{
+    class CircularList<T> : IEnumerable<T>
+    {
+        public Node<T> First { get; private set; }
+        public Node<T> Last { get; private set; }
+        public Node<T> Current { get; private set; }
+        public int Count { get; private set; }
+
+        public void Add(T value)
+        {
+            // Empty list, insert first node
+            if (First == null)
+            {
+                First = new Node<T> { Value = value };
+                Count++;
+                First.Next = First;
+                First.Previous = First;
+                Last = First;
+                Current = First;
+                return;
+            }
+
+            // Non-empty list, add to end of list
+            Last.Next = new Node<T>
+            {
+                Value = value,
+                Next = First,
+                Previous = Last
+            };
+            Count++;
+            Last = Last.Next;
+            First.Previous = Last;
+        }
+
+        public void Step()
+        {
+            Current = Current.Next;
+        }
+
+        public void Step(int steps)
+        {
+            for (int i = 0; i < steps; i++)
+            {
+                Step();
+            }
+        }
+
+        public void RemoveCurrent()
+        {
+            var nextNode = Current.Next;
+            var prevNode = Current.Previous;
+
+            prevNode.Next = nextNode;
+            nextNode.Previous = prevNode;
+
+            // Check if we're removing First/Last
+            if (Current == First) { First = nextNode; }
+            if (Current == Last) { Last = prevNode; }
+
+            Current = nextNode;
+            Count--;
+        }
+        public IEnumerator<T> GetEnumerator()
+        {
+            Node<T> node = First;
+
+            do
+            {
+                yield return node.Value;
+                node = node.Next;
+            } while (node != First);
+
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
---
+++ Josephus/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Linq;
+
+namespace Josephus
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            JosephusValues(50);
+        }
+
+        private static void CircularListdemo()
+        {
+            var circle = new CircularList<int>();
+
+            foreach (int i in Enumerable.Range(1, 5))
+            {
+                circle.Add(i);
+            }
+
+            Console.WriteLine("Starting circle:");
+            Console.WriteLine(string.Join(", ", circle.ToArray()));
+            Console.WriteLine($"Current: {circle.Current.Value}");
+
+            Console.WriteLine("Advancing 3 steps");
+            circle.Step(3);
+            Console.WriteLine($"Current: {circle.Current.Value}");
+            Console.WriteLine("Removing current");
+            circle.RemoveCurrent();
+            Console.WriteLine($"Current: {circle.Current.Value}");
+
+            Console.WriteLine("Current circle:");
+            Console.WriteLine(string.Join(", ", circle.ToArray()));
+        }
+
+        private static void Josephus(int size, int steps = 1, int survivors = 1)
+        {
+            var circle = new CircularList<int>();
+            foreach (int i in Enumerable.Range(1, size))
+            {
+                circle.Add(i);
+            }
+
+            // Start the circus!
+            int counter = 0;
+            while (circle.Count > survivors)
+            {
+                circle.Step(steps);
+                circle.RemoveCurrent();
+                counter++;
+            }
+
+            Console.WriteLine($"{survivors} survivor(s) after {counter} steps:");
+            Console.WriteLine(string.Join(", ", circle.ToArray()));
+
+        }
+
+        private static void JosephusValues(int maxSize)
+        {
+            for (int i = 1; i <= maxSize; i++)
+            {
+                int pos = BinarySolution(i);
+                Console.WriteLine($"{i}: {pos}");
+            }
+        }
+
+        private static int BinarySolution(int n)
+        {
+            // Fulhack?
+            int msb = (int) Math.Log(n, 2);
+            int l = n - (int) Math.Pow(2, msb);
+
+            int pos = 2*l + 1;
+            return pos;
+        }
+    }
+}
